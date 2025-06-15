2a94ec5c3437a5e220f291d3261443da56da392c
DanielVNZ <123446671+DanielVNZ@users.noreply.github.com>
2025-06-15 17:49:15 +1200

    Add project files.

---
+++ Mod.cs
@@ -0,0 +1,44 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using HarmonyLib;
+using Game.UI;
+
+namespace DisableScreenshot
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(DisableScreenshot)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+        private Harmony harmony;
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            harmony = new Harmony("com.danielvnz.disablescreenshot");
+            harmony.PatchAll();
+            log.Info("Harmony patch applied to ScreenCaptureHelper.CaptureScreenshot.");
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+            harmony?.UnpatchSelf();
+            log.Info("Harmony patch removed.");
+        }
+    }
+
+    [HarmonyPatch(typeof(ScreenCaptureHelper), nameof(ScreenCaptureHelper.CaptureScreenshot))]
+    public static class ScreenCaptureHelperPatch
+    {
+        static bool Prefix()
+        {
+            Mod.log.Info("Blocked ScreenCaptureHelper.CaptureScreenshot()");
+            return false;
+        }
+    }
+}
